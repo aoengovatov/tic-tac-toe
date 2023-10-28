179c7c6a8dcdef84836da95acf73e4caa5a358ab
ALexEngo <a.o.engovatov@yandex.ru>
2023-10-28 21:59:12 +0400

    create design app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import './App.css';
+import styles from './app.module.css';
+
+const AppLayout = ({ name }) => (
+	<div className={styles.container}>
+		<div className={styles.appContainer}>
+			<div className={styles.title}>{name}</div>
+			<div className={styles.board}>
+				<div className={styles.boardItem}>1</div>
+				<div className={styles.boardItem}>2</div>
+				<div className={styles.boardItem}>3</div>
+				<div className={styles.boardItem}>4</div>
+				<div className={styles.boardItem}>5</div>
+				<div className={styles.boardItem}>6</div>
+				<div className={styles.boardItem}>7</div>
+				<div className={styles.boardItem}>8</div>
+				<div className={styles.boardItem}>9</div>
+			</div>
+			<div className={styles.status}>строка состояния</div>
+			<button className={styles.button}>новая игра</button>
+		</div>
+	</div>
+);
 
 export const App = () => {
-	return <div>Крестики-нолики</div>;
+	const name = 'Крестики - нолики';
+	return <AppLayout name={name} />;
 };
