f078ebc287bf4852bbd6a25277a4825a22c177bc
ALexEngo <a.o.engovatov@yandex.ru>
2023-10-29 14:24:33 +0400

    styles edit, add icons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import styles from './app.module.css';
+import crossImage from './cross.png';
+import zeroImage from './zero.png';
 
 const AppLayout = ({ name }) => (
-	<div className={styles.container}>
-		<div className={styles.appContainer}>
-			<div className={styles.title}>{name}</div>
-			<div className={styles.board}>
-				<div className={styles.boardItem}>1</div>
-				<div className={styles.boardItem}>2</div>
-				<div className={styles.boardItem}>3</div>
-				<div className={styles.boardItem}>4</div>
-				<div className={styles.boardItem}>5</div>
-				<div className={styles.boardItem}>6</div>
-				<div className={styles.boardItem}>7</div>
-				<div className={styles.boardItem}>8</div>
-				<div className={styles.boardItem}>9</div>
-			</div>
-			<div className={styles.status}>строка состояния</div>
-			<button className={styles.button}>новая игра</button>
-		</div>
-	</div>
+    <div className={styles.container}>
+        <div className={styles.appContainer}>
+            <div className={styles.title}>{name}</div>
+            <div className={styles.board}>
+                <div className={`${styles.boardItem} ${styles.winLine}`}>
+                    <img src={crossImage} alt="cross"></img>
+                </div>
+                <div className={styles.boardItem}>
+                    <img src={zeroImage} alt="zero"></img>
+                </div>
+                <div className={styles.boardItem}></div>
+                <div className={styles.boardItem}></div>
+                <div className={styles.boardItem}></div>
+                <div className={styles.boardItem}></div>
+                <div className={styles.boardItem}></div>
+                <div className={styles.boardItem}></div>
+                <div className={styles.boardItem}></div>
+            </div>
+            <div className={styles.status}>строка состояния</div>
+            <button className={styles.button}>новая игра</button>
+        </div>
+    </div>
 );
 
 export const App = () => {
-	const name = 'Крестики - нолики';
-	return <AppLayout name={name} />;
+    const name = 'Крестики - нолики';
+    return <AppLayout name={name} />;
 };
